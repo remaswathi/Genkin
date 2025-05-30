f9ea13674e916e6dbdddcba7d6ff37ff7411e880
remaswathi <swathir.ece405@gmail.com>
2025-05-30 15:51:24 +0530

    jenkins repone created

---
+++ src/test/java/pack1/ContactTest.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test(groups="smoketesting")
+	
+	public void createContactTest() {
+		System.out.println("excute creacte contact test");
+		
+	}
+}
---
+++ src/test/java/pack2/OrganizationTest.java
@@ -0,0 +1,25 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test(groups= {"Regression Testing"})
+	public void createorganization() {
+		String URL=System.getProperty("url"); 
+		String BROWSER=System.getProperty("browser"); 
+		String USERNAME=System.getProperty("username"); 
+		String PASSWORD=System.getProperty("password"); 
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createorganization");
+	}
+
+	@Test
+	public void createTest() {
+		System.out.println("execute create");
+	}
+
+}
